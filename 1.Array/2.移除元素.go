5b965427acd64903635451d93f5579633d9e1cba
chengfei <867540436@qq.com>
2023-04-27 16:03:15 +0800

    4.27

---
+++ 1.Array/2.移除元素.go
@@ -0,0 +1,64 @@
+package main
+
+/*
+*
+给你一个数组 nums和一个值 val，你需要 原地 移除所有数值等于val的元素，并返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+
+元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+*/
+func removeElement(nums []int, val int) int {
+	count := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			count++
+			continue
+		}
+		j := i + 1
+		for j < len(nums) && nums[j] == val {
+			j++
+		}
+		if j < len(nums) {
+			swap(nums, i, j)
+			count++
+		} else {
+			break
+		}
+	}
+	return count
+}
+
+func swap(nums []int, i int, j int) {
+	temp := nums[i]
+	nums[i] = nums[j]
+	nums[j] = temp
+}
+
+// 双指针
+func removeElement1(nums []int, val int) int {
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		if nums[left] == val {
+			nums[left] = nums[right]
+			right--
+		} else {
+			left++
+		}
+	}
+	return left
+}
+
+// 快慢指针
+func removeElement2(nums []int, val int) int {
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[fast] != val {
+			nums[slow] = nums[fast]
+			slow++
+		}
+		fast++
+	}
+	return slow
+}
